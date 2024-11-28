3550e675973edc947052427db52cffa43440db01
Alexander Senko <Alexander.Senko@gmail.com>
2024-11-28 07:37:07 +0700

    Framework: added `Kernel` extension helpers
    Use `Magic::CoreExt.kernel` to define `Kernel` extension methods.
    
    ```ruby
    Magic::CoreExt.kernel do
      def some_method
        …
      end
    end
    ```

---
+++ lib/magic/core_ext/kernel.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+require 'magic/core_ext'
+
+module Magic # :nodoc:
+	module CoreExt # :nodoc:
+		module Kernel
+			# Some classes copy Kernel methods on initialization instead of
+			# including Kernel itself. Thus, new methods should be defined
+			# explicitly for them.
+			MODULES = [ # classes/modules including Kernel methods
+					::Kernel,
+					(Delegator if defined? Delegator),
+			].compact.freeze
+		end
+
+		module_function
+
+		def kernel(&)
+			Module.new(&).tap do |kernel|
+				Kernel::MODULES.each { _1.include kernel }
+			end
+		end
+	end
+
+	CoreExt.require_all __FILE__
+end
---
+++ spec/magic/core_ext/kernel_spec.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+require 'magic/core_ext/kernel'
+require 'active_support/core_ext/enumerable'
+
+RSpec.describe Kernel do
+	subject { Object.new }
+
+	let(:object) { Object.new }
+end
+
+# Every method defined on `Kernel` should be defined on some other
+# classes as well.
+# See `Magic::CoreExt::Kernel::MODULES` for further details.
+RSpec.shared_context full_description: /^Kernel#/ do
+	let(:kernel_method) { Kernel.instance_method method_name }
+
+	for including_kernel in Magic::CoreExt::Kernel::MODULES.without Kernel do
+		describe including_kernel do
+			it 'has the same method' do
+				expect(described_class.instance_method method_name).to eq kernel_method
+			end
+		end
+	end
+end
