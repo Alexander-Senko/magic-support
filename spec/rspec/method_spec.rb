Alexander-Senko/magic-support

commit e7d9791a1ad4416f5ad6c260217f88e4a29fa75b

API / RSpec: improved method specs
* Extracted `receiver` into an explicit `let`.
* Added warnings on a missing method.
* Don’t swallow unrelated errors.

lib/magic/support/version.rb
@@ -2,6 +2,6 @@
 
 module Magic
 	module Support
-		VERSION = '0.1.0'
+		VERSION = '0.2.0.alpha'
 	end
 end

lib/rspec/method.rb
@@ -30,22 +30,15 @@ module RSpec # :nodoc:
 					subject_method = subject_method.super_method or break
 				end
 
-				define_method :subject do
-					receiver =
-							case [ scope, subject_method&.bind_call(self) || super() ]
-							in '#', receiver
-								receiver
-							in '.', Module => receiver
-								receiver
-							in '.', receiver
-								receiver.class
-							end
-
-					receiver.method name
-				rescue NameError
-					# TODO: emit a warning
-
-					-> *args { receiver.send name, *args }
+				let :receiver do
+					case [ scope, subject_method&.bind_call(self) || super() ]
+					in '#', receiver
+						receiver
+					in '.', Module => receiver
+						receiver
+					in '.', receiver
+						receiver.class
+					end
 				end
 
 				let(:method_name) { name.to_sym }
@@ -61,6 +54,17 @@ module RSpec # :nodoc:
 				next unless defined? RSpec::Its
 
 				def its_result(*args, &) = its(args, &)
+			end
+
+			def subject
+				receiver.method method_name
+			rescue NameError => error
+				raise unless
+						(receiver.is_a? error.receiver rescue false)
+
+				warn "Testing #{error}"
+
+				-> *args { receiver.send method_name, *args }
 			end
 		end
 	end

spec/rspec/method_spec.rb
@@ -8,15 +8,19 @@ RSpec.describe RSpec::Method do
 	end
 
 	describe ':method context' do
-		subject { Object.new }
+		subject { object }
+
+		let(:object) { Object.new }
 
 		describe '.new' do
+			it { expect(receiver).to eq object.class }
 			it { expect(method_name).to eq :new }
 
 			its_result { is_expected.to be_instance_of Object }
 		end
 
 		describe '#to_s' do
+			it { expect(receiver).to eq object }
 			it { expect(method_name).to eq :to_s }
 
 			its_result { is_expected.to be_a String }
@@ -32,6 +36,7 @@ RSpec.describe RSpec::Method do
 
 		context 'when private' do
 			describe '#puts' do
+				it { expect(receiver).to eq object }
 				it { expect(method_name).to eq :puts }
 
 				its_result { is_expected.to be_nil }
@@ -39,10 +44,39 @@ RSpec.describe RSpec::Method do
 		end
 
 		context 'when missing' do
+			let :have_emitted_warning do
+				have_received(:warn)
+						.with(/Testing undefined method/)
+			end
+
+			before { allow(self).to receive :warn }
+
 			describe '#undefined_method' do
+				it { expect(receiver).to eq object }
 				it { expect(method_name).to eq :undefined_method }
 
 				it { expect { subject[] }.to raise_error NoMethodError }
+				it { expect { subject   }.not_to raise_error }
+
+				it 'emits a warning' do
+					subject
+
+					expect(self).to have_emitted_warning
+							.once
+				end
+
+				context 'with bugs' do
+					let(:method_name) { name }
+
+					it { expect { subject[] }.to raise_error NoMethodError }
+					it { expect { subject   }.to raise_error NoMethodError }
+
+					it 'doesn’t emit a warning' do
+						subject rescue nil
+
+						expect(self).not_to have_emitted_warning
+					end
+				end
 			end
 		end
 
@@ -50,6 +84,9 @@ RSpec.describe RSpec::Method do
 			subject { Object }
 
 			shared_examples '.new' do
+				it { expect(receiver).to eq Object }
+				it { expect(method_name).to eq :new }
+
 				its_result { is_expected.to be_instance_of Object }
 			end
 
@@ -62,6 +99,9 @@ RSpec.describe RSpec::Method do
 			subject { Kernel }
 
 			shared_examples '.pp' do
+				it { expect(receiver).to eq Kernel }
+				it { expect(method_name).to eq :pp }
+
 				its_result                                  { is_expected.to be_nil }
 				its_result(argument = Object.new)           { is_expected.to eq argument }
 				its(arguments = 2.times.map { Object.new }) { is_expected.to eq arguments }

sig/rspec/method.rbs
@@ -1,5 +1,9 @@
 module RSpec
 	module Method
 		REFERENCE: Regexp
+
+		module ExampleGroup
+			def subject: () -> (::Method | Proc)
+		end
 	end
 end

spec/magic/core_ext/kernel_spec.rb
@@ -23,7 +23,7 @@ RSpec.describe Kernel do
 			let(:condition) { nil }
 
 			it 'returns self' do
-				expect(subject.(&block)).to be subject.receiver
+				expect(subject.(&block)).to be receiver
 			end
 		end
 
@@ -31,7 +31,7 @@ RSpec.describe Kernel do
 			let(:condition) { false }
 
 			it 'returns self' do
-				expect(subject.(&block)).to be subject.receiver
+				expect(subject.(&block)).to be receiver
 			end
 		end
 	end

commit 8804592e7ef2fcf9afa48f8ca4a8292e357cc70e

API / RSpec: fixed a regression
Method specs with implicit subjects got broken.

Broken in: e7d9791a.

lib/magic/support/version.rb
@@ -2,6 +2,6 @@
 
 module Magic
 	module Support
-		VERSION = '0.2.0'
+		VERSION = '0.2.1'
 	end
 end

lib/rspec/method.rb
@@ -30,8 +30,14 @@ module RSpec # :nodoc:
 					subject_method = subject_method.super_method or break
 				end
 
+				if subject_method
+					let(:__subject__) { subject_method.bind_call self }
+				else
+					let(:__subject__) { method(:subject).super_method[] }
+				end
+
 				let :receiver do
-					case [ scope, subject_method&.bind_call(self) || super() ]
+					case [ scope, __subject__ ]
 					in '#', receiver
 						receiver
 					in '.', Module => receiver

spec/rspec/method_spec.rb
@@ -8,108 +8,120 @@ RSpec.describe RSpec::Method do
 	end
 
 	describe ':method context' do
-		subject { object }
+		context 'with an implicit subject' do
+			describe '.to_s' do
+				it { expect(receiver).to eq described_class }
+				it { expect(method_name).to eq :to_s }
 
-		let(:object) { Object.new }
-
-		describe '.new' do
-			it { expect(receiver).to eq object.class }
-			it { expect(method_name).to eq :new }
-
-			its_result { is_expected.to be_instance_of Object }
-		end
-
-		describe '#to_s' do
-			it { expect(receiver).to eq object }
-			it { expect(method_name).to eq :to_s }
-
-			its_result { is_expected.to be_a String }
-			its_result { is_expected.to match /\A#<Object:0x\h+>\Z/ }
-
-			context 'with an overridden subject' do
-				subject { :new_subject }
-
-				# NOTE: it’s still `Object#to_s`, not `Symbol#to_s`
-				its_result { is_expected.to match /\A#<Symbol:0x\h+>\Z/ }
+				its_result { is_expected.to be_a String }
+				its_result { is_expected.to eq described_class.name }
 			end
 		end
 
-		context 'when private' do
-			describe '#puts' do
-				it { expect(receiver).to eq object }
-				it { expect(method_name).to eq :puts }
+		context 'with an explicit subject' do
+			subject { object }
 
-				its_result { is_expected.to be_nil }
-			end
-		end
+			let(:object) { Object.new }
 
-		context 'when missing' do
-			let :have_emitted_warning do
-				have_received(:warn)
-						.with(/Testing undefined method/)
-			end
-
-			before { allow(self).to receive :warn }
-
-			describe '#undefined_method' do
-				it { expect(receiver).to eq object }
-				it { expect(method_name).to eq :undefined_method }
-
-				it { expect { subject[] }.to raise_error NoMethodError }
-				it { expect { subject   }.not_to raise_error }
-
-				it 'emits a warning' do
-					subject
-
-					expect(self).to have_emitted_warning
-							.once
-				end
-
-				context 'with bugs' do
-					let(:method_name) { name }
-
-					it { expect { subject[] }.to raise_error NoMethodError }
-					it { expect { subject   }.to raise_error NoMethodError }
-
-					it 'doesn’t emit a warning' do
-						subject rescue nil
-
-						expect(self).not_to have_emitted_warning
-					end
-				end
-			end
-		end
-
-		context 'with a class' do
-			subject { Object }
-
-			shared_examples '.new' do
-				it { expect(receiver).to eq Object }
+			describe '.new' do
+				it { expect(receiver).to eq object.class }
 				it { expect(method_name).to eq :new }
 
 				its_result { is_expected.to be_instance_of Object }
 			end
 
-			describe('.new') { it_behaves_like '.new' }
+			describe '#to_s' do
+				it { expect(receiver).to eq object }
+				it { expect(method_name).to eq :to_s }
 
-			describe('#new') { it_behaves_like '.new' }
-		end
+				its_result { is_expected.to be_a String }
+				its_result { is_expected.to match /\A#<Object:0x\h+>\Z/ }
 
-		context 'with a module' do
-			subject { Kernel }
+				context 'with an overridden subject' do
+					subject { :new_subject }
 
-			shared_examples '.pp' do
-				it { expect(receiver).to eq Kernel }
-				it { expect(method_name).to eq :pp }
-
-				its_result                                  { is_expected.to be_nil }
-				its_result(argument = Object.new)           { is_expected.to eq argument }
-				its(arguments = 2.times.map { Object.new }) { is_expected.to eq arguments }
+					# NOTE: it’s still `Object#to_s`, not `Symbol#to_s`
+					its_result { is_expected.to match /\A#<Symbol:0x\h+>\Z/ }
+				end
 			end
 
-			describe('.pp') { it_behaves_like '.pp' }
+			context 'when private' do
+				describe '#puts' do
+					it { expect(receiver).to eq object }
+					it { expect(method_name).to eq :puts }
 
-			describe('#pp') { it_behaves_like '.pp' }
+					its_result { is_expected.to be_nil }
+				end
+			end
+
+			context 'when missing' do
+				let :have_emitted_warning do
+					have_received(:warn)
+							.with(/Testing undefined method/)
+				end
+
+				before { allow(self).to receive :warn }
+
+				describe '#undefined_method' do
+					it { expect(receiver).to eq object }
+					it { expect(method_name).to eq :undefined_method }
+
+					it { expect { subject[] }.to raise_error NoMethodError }
+					it { expect { subject   }.not_to raise_error }
+
+					it 'emits a warning' do
+						subject
+
+						expect(self).to have_emitted_warning
+								.once
+					end
+
+					context 'with bugs' do
+						let(:method_name) { name }
+
+						it { expect { subject[] }.to raise_error NoMethodError }
+						it { expect { subject   }.to raise_error NoMethodError }
+
+						it 'doesn’t emit a warning' do
+							subject rescue nil
+
+							expect(self).not_to have_emitted_warning
+						end
+					end
+				end
+			end
+
+			context 'with a class' do
+				subject { Object }
+
+				shared_examples '.new' do
+					it { expect(receiver).to eq Object }
+					it { expect(method_name).to eq :new }
+
+					its_result { is_expected.to be_instance_of Object }
+				end
+
+				describe('.new') { it_behaves_like '.new' }
+
+				describe('#new') { it_behaves_like '.new' }
+			end
+
+			context 'with a module' do
+				subject { Kernel }
+
+				shared_examples '.pp' do
+					it { expect(receiver).to eq Kernel }
+					it { expect(method_name).to eq :pp }
+
+					its_result                                  { is_expected.to be_nil }
+					its_result(argument = Object.new)           { is_expected.to eq argument }
+					its(arguments = 2.times.map { Object.new }) { is_expected.to eq arguments }
+				end
+
+				describe('.pp') { it_behaves_like '.pp' }
+
+				describe('#pp') { it_behaves_like '.pp' }
+			end
 		end
 
 		describe ':delegated context' do