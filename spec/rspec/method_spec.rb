Alexander-Senko/magic-support

commit 8dbc8c5e28887c88c840204daf658ce80c9473df

API / RSpec: support method specs nesting
This could be useful when a method being tested affects other methods.
See the spec for examples.

Altered some tests to match new behavior.

lib/rspec/method.rb
@@ -11,7 +11,7 @@ module RSpec # :nodoc:
 		}
 				.map { |name, pattern| "(?<#{name}>#{pattern})" }
 				.join
-				.then { /(?:^|\s)#{_1}(?:$|\s)/ } # may be surrounded by some text
+				.then { /(?:^|\s)#{_1}\b/ } # may be surrounded by some text
 
 		# @api public
 		# Container module for method specs.
@@ -100,6 +100,12 @@ module RSpec # :nodoc:
 						.once
 			end
 		end
+	end
+
+	shared_context :nested_method, full_description: /(.*#{Method::REFERENCE}){2,}/ do
+		next unless metadata[:description].match? Method::REFERENCE
+
+		setup_method_context
 	end
 
 	configure do

spec/rspec/method_spec.rb
@@ -94,33 +94,44 @@ RSpec.describe RSpec::Method do
 			context 'with a class' do
 				subject { Object }
 
-				shared_examples '.new' do
+				shared_examples 'new' do
 					it { expect(receiver).to eq Object }
 					it { expect(method_name).to eq :new }
 
 					its_result { is_expected.to be_instance_of Object }
 				end
 
-				describe('.new') { it_behaves_like '.new' }
+				describe('.new') { it_behaves_like 'new' }
 
-				describe('#new') { it_behaves_like '.new' }
+				describe('#new') { it_behaves_like 'new' }
 			end
 
 			context 'with a module' do
 				subject { Kernel }
 
-				shared_examples '.pp' do
+				shared_examples 'pp' do
 					it { expect(receiver).to eq Kernel }
 					it { expect(method_name).to eq :pp }
 
-					its_result                                  { is_expected.to be_nil }
-					its_result(argument = Object.new)           { is_expected.to eq argument }
-					its(arguments = 2.times.map { Object.new }) { is_expected.to eq arguments }
+					its_result                        { is_expected.to be_nil }
+					its_result(argument = Object.new) { is_expected.to eq argument }
 				end
 
-				describe('.pp') { it_behaves_like '.pp' }
+				describe('.pp') { it_behaves_like 'pp' }
 
-				describe('#pp') { it_behaves_like '.pp' }
+				describe('#pp') { it_behaves_like 'pp' }
+			end
+		end
+
+		context 'when nested' do
+			describe '.define_method' do
+				before { receiver.class.define_method method_name, &block }
+
+				describe '#my_method' do
+					let(:block) { -> { :result } }
+
+					its_result { is_expected.to eq :result }
+				end
 			end
 		end
 

commit 7d40ff89d962e3ced6a201ee37bb63fa28e5390d

API / RSpec: fixed a regression
Specs for methods with punctuation suffixes got broken.

Rearranged tests a bit as well.

Broken in: 8dbc8c5e.

lib/magic/support/version.rb
@@ -2,6 +2,6 @@
 
 module Magic
 	module Support
-		VERSION = '0.3.0'
+		VERSION = '0.3.1'
 	end
 end

lib/rspec/method.rb
@@ -11,7 +11,7 @@ module RSpec # :nodoc:
 		}
 				.map { |name, pattern| "(?<#{name}>#{pattern})" }
 				.join
-				.then { /(?:^|\s)#{_1}\b/ } # may be surrounded by some text
+				.then { /(?:^|\s)#{_1}(?:$|\s)?/ } # may be surrounded by some text
 
 		# @api public
 		# Container module for method specs.

spec/rspec/method_spec.rb
@@ -55,52 +55,6 @@ RSpec.describe RSpec::Method do
 				end
 			end
 
-			context 'when private' do
-				describe '#puts' do
-					it { expect(receiver).to eq object }
-					it { expect(method_name).to eq :puts }
-
-					its_result { is_expected.to be_nil }
-				end
-			end
-
-			context 'when missing' do
-				let :have_emitted_warning do
-					have_received(:warn)
-							.with(/Testing undefined method/)
-				end
-
-				before { allow(self).to receive :warn }
-
-				describe '#undefined_method' do
-					it { expect(receiver).to eq object }
-					it { expect(method_name).to eq :undefined_method }
-
-					it { expect { subject[] }.to raise_error NoMethodError }
-					it { expect { subject   }.not_to raise_error }
-
-					it 'emits a warning' do
-						subject
-
-						expect(self).to have_emitted_warning
-								.once
-					end
-
-					context 'with bugs' do
-						let(:method_name) { name }
-
-						it { expect { subject[] }.to raise_error NoMethodError }
-						it { expect { subject   }.to raise_error NoMethodError }
-
-						it 'doesn’t emit a warning' do
-							subject rescue nil
-
-							expect(self).not_to have_emitted_warning
-						end
-					end
-				end
-			end
-
 			context 'with a class' do
 				subject { Object }
 
@@ -130,6 +84,64 @@ RSpec.describe RSpec::Method do
 				describe('.pp') { it_behaves_like 'pp' }
 
 				describe('#pp') { it_behaves_like 'pp' }
+			end
+		end
+
+		context 'with punctuation suffixes' do
+			describe '#nil?' do
+				it { is_expected.to be_a Method }
+				it { expect(receiver).to eq subject.receiver }
+				it { expect(method_name).to eq :nil? }
+
+				its_result { is_expected.to be false }
+			end
+		end
+
+		context 'when private' do
+			describe '#puts' do
+				it { is_expected.to be_a Method }
+				it { expect(receiver).to eq subject.receiver }
+				it { expect(method_name).to eq :puts }
+
+				its_result { is_expected.to be_nil }
+			end
+		end
+
+		context 'when missing' do
+			let :have_emitted_warning do
+				have_received(:warn)
+						.with(/Testing undefined method/)
+			end
+
+			before { allow(self).to receive :warn }
+
+			describe '#undefined_method' do
+				it { is_expected.to be_a Proc }
+				it { expect(receiver).to eq described_class }
+				it { expect(method_name).to eq :undefined_method }
+
+				it { expect { subject.call }.to raise_error NoMethodError }
+				it { expect { subject      }.not_to raise_error }
+
+				it 'emits a warning' do
+					subject
+
+					expect(self).to have_emitted_warning
+							.once
+				end
+
+				context 'with bugs' do
+					let(:method_name) { name }
+
+					it { expect { subject.call }.to raise_error NoMethodError }
+					it { expect { subject      }.to raise_error NoMethodError }
+
+					it 'doesn’t emit a warning' do
+						subject rescue nil
+
+						expect(self).not_to have_emitted_warning
+					end
+				end
 			end
 		end
 